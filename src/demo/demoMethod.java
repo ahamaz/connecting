c3d67d6801626a89ec9b146c4e819f29ecc9791c
ahamaz <ahasanfsm@hotmail.com>
2021-11-15 11:23:07 +0000

    project1

---
+++ src/demo/demoMethod.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+
+public class demoMethod {
+void demomethod( ){
+	Integer [ ] myArray = new Integer[10];
+	try
+	for (int nt = 0; nt < myArray.length; nt++) {
+		Integer integer = myArray[nt];
+		
+	}
+}
+
+
+}
---
+++ src/helloworld/Main.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class Main {
+	public static void main(String[] arg) {
+		System.out.println("Hello world");	
+	}
+	
+void foo() {
+	int i = 0;
+	if (i==5) {
+		 
+	}
+	
+	
+}
+}
